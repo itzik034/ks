c60fa0a2950ba2cba489405dcf49606dca12abca
Itzik <itzik.king034@gmail.com>
2024-02-20 23:44:59 +0200

    record page updated

--- layout/records.php
+++ layout/records.php
@@ -12,5 +12,347 @@
         <span>David</span>
         <span>David Shmuelovich</span>
         <span>David</span>
+        <div class="rc_plus">+</div>
+    </div>
+    <div class="homework">
+        <div class="rc_col">
+            <div class="rc_row rc_title">
+                Title
+            </div>
+            <div class="rc_scroll">
+                <div class="rc_row">
+                    <div class="rc_row_pic">
+                        <img src="img/user.png">
+                    </div>
+                    <div class="rc_row_text">
+                        <div class="rc_row_text1">
+                            Title
+                        </div>
+                        <div class="rc_row_text2">
+                            <div class="rc_text2_num">
+                                3
+                            </div>
+                            <div class="rc_text2_prog">
+                                70%
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="rc_row">
+                    <div class="rc_row_pic">
+                        <img src="img/user.png">
+                    </div>
+                    <div class="rc_row_text">
+                        <div class="rc_row_text1">
+                            Title
+                        </div>
+                        <div class="rc_row_text2">
+                            <div class="rc_text2_num">
+                                3
+                            </div>
+                            <div class="rc_text2_prog">
+                                70%
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="rc_row">
+                    <div class="rc_row_pic">
+                        <img src="img/user.png">
+                    </div>
+                    <div class="rc_row_text">
+                        <div class="rc_row_text1">
+                            Title
+                        </div>
+                        <div class="rc_row_text2">
+                            <div class="rc_text2_num">
+                                3
+                            </div>
+                            <div class="rc_text2_prog">
+                                70%
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="rc_row">
+                    <div class="rc_row_pic">
+                        <img src="img/user.png">
+                    </div>
+                    <div class="rc_row_text">
+                        <div class="rc_row_text1">
+                            Title
+                        </div>
+                        <div class="rc_row_text2">
+                            <div class="rc_text2_num">
+                                3
+                            </div>
+                            <div class="rc_text2_prog">
+                                70%
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="rc_plus">+</div>
+            </div>
+        </div>
+
+        <div class="rc_col">
+            <div class="rc_row rc_title">
+                Title
+            </div>
+            <div class="rc_scroll">
+                <div class="rc_row">
+                    <div class="rc_row_pic">
+                        <img src="img/user.png">
+                    </div>
+                    <div class="rc_row_text">
+                        <div class="rc_row_text1">
+                            Title
+                        </div>
+                        <div class="rc_row_text2">
+                            <div class="rc_text2_num">
+                                3
+                            </div>
+                            <div class="rc_text2_prog">
+                                70%
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="rc_row">
+                    <div class="rc_row_pic">
+                        <img src="img/user.png">
+                    </div>
+                    <div class="rc_row_text">
+                        <div class="rc_row_text1">
+                            Title
+                        </div>
+                        <div class="rc_row_text2">
+                            <div class="rc_text2_num">
+                                3
+                            </div>
+                            <div class="rc_text2_prog">
+                                70%
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="rc_row">
+                    <div class="rc_row_pic">
+                        <img src="img/user.png">
+                    </div>
+                    <div class="rc_row_text">
+                        <div class="rc_row_text1">
+                            Title
+                        </div>
+                        <div class="rc_row_text2">
+                            <div class="rc_text2_num">
+                                3
+                            </div>
+                            <div class="rc_text2_prog">
+                                70%
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="rc_row">
+                    <div class="rc_row_pic">
+                        <img src="img/user.png">
+                    </div>
+                    <div class="rc_row_text">
+                        <div class="rc_row_text1">
+                            Title
+                        </div>
+                        <div class="rc_row_text2">
+                            <div class="rc_text2_num">
+                                3
+                            </div>
+                            <div class="rc_text2_prog">
+                                70%
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="rc_plus">+</div>
+            </div>
+        </div>
+
+        <div class="rc_col">
+            <div class="rc_row rc_title">
+                Title
+            </div>
+            <div class="rc_scroll">
+                <div class="rc_row">
+                    <div class="rc_row_pic">
+                        <img src="img/user.png">
+                    </div>
+                    <div class="rc_row_text">
+                        <div class="rc_row_text1">
+                            Title
+                        </div>
+                        <div class="rc_row_text2">
+                            <div class="rc_text2_num">
+                                3
+                            </div>
+                            <div class="rc_text2_prog">
+                                70%
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="rc_row">
+                    <div class="rc_row_pic">
+                        <img src="img/user.png">
+                    </div>
+                    <div class="rc_row_text">
+                        <div class="rc_row_text1">
+                            Title
+                        </div>
+                        <div class="rc_row_text2">
+                            <div class="rc_text2_num">
+                                3
+                            </div>
+                            <div class="rc_text2_prog">
+                                70%
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="rc_row">
+                    <div class="rc_row_pic">
+                        <img src="img/user.png">
+                    </div>
+                    <div class="rc_row_text">
+                        <div class="rc_row_text1">
+                            Title
+                        </div>
+                        <div class="rc_row_text2">
+                            <div class="rc_text2_num">
+                                3
+                            </div>
+                            <div class="rc_text2_prog">
+                                70%
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="rc_row">
+                    <div class="rc_row_pic">
+                        <img src="img/user.png">
+                    </div>
+                    <div class="rc_row_text">
+                        <div class="rc_row_text1">
+                            Title
+                        </div>
+                        <div class="rc_row_text2">
+                            <div class="rc_text2_num">
+                                3
+                            </div>
+                            <div class="rc_text2_prog">
+                                70%
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="rc_plus">+</div>
+            </div>
+        </div>
+
+        <div class="rc_col">
+            <div class="rc_row rc_title">
+                Title
+            </div>
+            <div class="rc_scroll">
+                <div class="rc_row">
+                    <div class="rc_row_pic">
+                        <img src="img/user.png">
+                    </div>
+                    <div class="rc_row_text">
+                        <div class="rc_row_text1">
+                            Title
+                        </div>
+                        <div class="rc_row_text2">
+                            <div class="rc_text2_num">
+                                3
+                            </div>
+                            <div class="rc_text2_prog">
+                                70%
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="rc_row">
+                    <div class="rc_row_pic">
+                        <img src="img/user.png">
+                    </div>
+                    <div class="rc_row_text">
+                        <div class="rc_row_text1">
+                            Title
+                        </div>
+                        <div class="rc_row_text2">
+                            <div class="rc_text2_num">
+                                3
+                            </div>
+                            <div class="rc_text2_prog">
+                                70%
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="rc_row">
+                    <div class="rc_row_pic">
+                        <img src="img/user.png">
+                    </div>
+                    <div class="rc_row_text">
+                        <div class="rc_row_text1">
+                            Title
+                        </div>
+                        <div class="rc_row_text2">
+                            <div class="rc_text2_num">
+                                3
+                            </div>
+                            <div class="rc_text2_prog">
+                                70%
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="rc_row">
+                    <div class="rc_row_pic">
+                        <img src="img/user.png">
+                    </div>
+                    <div class="rc_row_text">
+                        <div class="rc_row_text1">
+                            Title
+                        </div>
+                        <div class="rc_row_text2">
+                            <div class="rc_text2_num">
+                                3
+                            </div>
+                            <div class="rc_text2_prog">
+                                70%
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="rc_plus">+</div>
+            </div>
+        </div>
     </div>
 </div>
